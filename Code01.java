914c37a63c5eec135699b437d348e5071af480e5
hopago <132269386+hopago@users.noreply.github.com>
2024-04-08 18:48:16 +0900

    first commit

---
+++ Code01.java
@@ -0,0 +1,16 @@
+/*
+  void func(...) {
+    func(...);
+  }
+*/
+
+public class Code01 {
+  public static void main(String[] args) {
+    func();
+  }
+
+  public static void func() {
+    System.out.println("Hello...");
+    func();
+  }
+}
